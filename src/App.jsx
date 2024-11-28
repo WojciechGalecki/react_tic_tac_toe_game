4b0d4886bc262b3fa8fb00b24bf909dca5878ca9
Wojciech Galecki <wojciech.galecki@wp.pl>
2024-11-28 13:13:39 +0100

    finished the end game

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
 
 const playerX = "X";
 const playerO = "O";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = derivedActivePlayer(gameTurns);
+  const gameBoard = getGameBoard(gameTurns);
+  const winner = derivedWinner(gameBoard);
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -39,21 +54,60 @@ function App() {
             isActive={activePlayer === playerO}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
   );
 }
 
-function derivedActivePlayer(turns) {
+function derivedActivePlayer(gameTurns) {
   let activePlayer = playerX;
 
-  if (turns.length > 0 && turns[0].player === playerX) {
+  if (gameTurns.length > 0 && gameTurns[0].player === playerX) {
     activePlayer = playerO;
   }
 
   return activePlayer;
+}
+
+function getGameBoard(gameTurns) {
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
+
+function derivedWinner(gameBoard) {
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  return winner;
 }
 
 export default App;
