4b8a26aea67494e1ff78f2354b48edda543ca48a
Wojciech Galecki <wojciech.galecki@wp.pl>
2024-11-28 12:13:17 +0100

    reduced states

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,59 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
+
+const playerX = "X";
+const playerO = "O";
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectSquare() {
-    setActivePlayer((currentlyActivePlayer) =>
-      currentlyActivePlayer === "X" ? "O" : "X"
-    );
+  const activePlayer = derivedActivePlayer(gameTurns);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => {
+      const currentPlayer = derivedActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
-          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
+          <Player
+            name="Player 1"
+            symbol={playerX}
+            isActive={activePlayer === playerX}
+          />
+          <Player
+            name="Player 2"
+            symbol={playerO}
+            isActive={activePlayer === playerO}
+          />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   );
+}
+
+function derivedActivePlayer(turns) {
+  let activePlayer = playerX;
+
+  if (turns.length > 0 && turns[0].player === playerX) {
+    activePlayer = playerO;
+  }
+
+  return activePlayer;
 }
 
 export default App;
